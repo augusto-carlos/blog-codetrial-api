ed970d1381cd22bce555da0b781a62bb0f0c32ba
Augusto Carlos <augustocarlos9966@gmail.com>
2020-05-28 00:28:54 +0100

    removendo a variável DATABASE_URL

--- config/database.js
+++ config/database.js
@@ -6,10 +6,6 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
-const DATABASE_URL = Env.get('DATABASE_URL')
-
-console.log(DATABASE_URL.username)
-
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -74,25 +70,14 @@ module.exports = {
   | npm i --save pg
   |
   */
-  // pg: {
-  //   client: 'pg',
-  //   connection: {
-  //     host: Env.get('DB_HOST', 'localhost'),
-  //     port: Env.get('DB_PORT', ''),
-  //     user: Env.get('DB_USER', 'root'),
-  //     password: Env.get('DB_PASSWORD', ''),
-  //     database: Env.get('DB_DATABASE', 'adonis')
-  //   },
-  //   debug: Env.get('DB_DEBUG', false)
-  // }
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.hostname),
-      port: Env.get('DB_PORT', DATABASE_URL.port),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', '5432'),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
     },
     debug: Env.get('DB_DEBUG', false)
   }
